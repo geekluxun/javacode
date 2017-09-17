56ed49fbf8e194dc614008545481a69568eeb2aa
geekluxun <geekluxun@163.com>
2017-09-17 08:58:14 +0800

    java 编程思想

---
+++ src/main/java/JavacodeApplication.java
@@ -0,0 +1,12 @@
+package com.geekluxun.www.javacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacodeApplication.class, args);
+	}
+}
---
+++ src/main/java/typeinfo/InterfaceViolation.java
@@ -0,0 +1,25 @@
+//: typeinfo/InterfaceViolation.java
+// Sneaking around an interface.
+package typeinfo;
+
+import typeinfo.interfacea.*;
+
+class B3 implements A {
+  public void f() {}
+  public void g() {}
+}
+
+public class InterfaceViolation {
+  public static void main(String[] args) {
+    A a = new B3();
+    a.f();
+    // a.g(); // Compile error
+    System.out.println(a.getClass().getName());
+    if(a instanceof B3) {
+      B3 b = (B3)a;
+      b.g();
+    }
+  }
+} /* Output:
+B
+*///:~
